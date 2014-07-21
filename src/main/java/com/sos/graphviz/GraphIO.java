ea7bb047e4c09b94056df7e67d8fcfa3d23ec675
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2014-07-21 09:20:42 +0000

    -
    svn-id 26438

--- src/main/java/com/sos/graphviz/GraphIO.java
+++ src/main/java/com/sos/graphviz/GraphIO.java
@@ -1,34 +1,49 @@
 package com.sos.graphviz;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import com.google.common.io.Files;
-import com.sos.graphviz.enums.FileType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import com.google.common.io.Files;
+import com.sos.JSHelper.Exceptions.JobSchedulerException;
+import com.sos.graphviz.enums.FileType;
 
 public class GraphIO {
-	
-	private static final Logger logger = LoggerFactory.getLogger(GraphIO.class);
-
-    public static final String name = System.getProperty("os.name");
-    public static final boolean isWindows = name.startsWith("Windows");
-	private static final String DOT = isWindows ? "dot.exe" : "dot";
-	
-	private String tempDir = System.getProperty("java.io.tmpdir");
-    private String dotDir = null;
-
-    private final Graph graph;
+	private static final Logger	logger		= LoggerFactory.getLogger(GraphIO.class);
+	public static final String	name		= System.getProperty("os.name");
+	public static final boolean	isWindows	= name.startsWith("Windows");
+	private static String		DOT			= isWindows ? "dot.exe" : "dot";
+	private String				tempDir		= System.getProperty("java.io.tmpdir");
+	private String				dotDir		= null;
+	private final Graph			graph;
 
 	public GraphIO(Graph graph) {
 		this.graph = graph;
 	}
 
-	/**
+	public void setDotExecFileName(final String pstrDotExecFileName) {
+		if (new File(pstrDotExecFileName).exists() == false) {
+			throw new JobSchedulerException(String.format("Dot-executable '%1$s' not found", pstrDotExecFileName));
+		}
+		DOT = pstrDotExecFileName;
+	}
+
+	public String getDotExecFileName() {
+		return DOT;
+	}
+
+/**
 	 * Writes the graph's image in a file.
 
-     * @param  type The type of file.
-     * @param  file A Filename to where we want to write.
+	 * @param  type The type of file.
+	 * @param  file A Filename to where we want to write.
 	 */
 	public void writeGraphToFile(FileType type, String file) throws IOException {
 		File to = new File(file);
@@ -44,40 +59,39 @@ public class GraphIO {
 	public void writeGraphToFile(FileType type, File to) throws IOException {
 		logger.debug("Write output to file " + to.getAbsolutePath() + ".");
 		FileOutputStream fos = new FileOutputStream(to);
-        File dot = writeDotSourceToTemporaryFile(graph.getSource());
-		fos.write(getGraph(dot,type));
+		File dot = writeDotSourceToTemporaryFile(graph.getSource());
+		fos.write(getGraph(dot, type));
 		fos.close();
-        if(getDotDir()!=null) {
-            // String targetName = replaceLast(to.getName(),Files.getFileExtension(to.getName()),"dot"); // getFileExtension nicht in Guava 10,0
-            String targetName = replaceLast(to.getName(),getFileExtension(to.getName()),"dot");
-            File targetFile = new File(getDotDir(),targetName);
-            logger.debug("try to move {} to {}.", dot.getAbsolutePath(), targetFile.getAbsolutePath());
-            Files.move(dot, targetFile);
-        } else
-            deleteDotFile(dot);
+		if (getDotDir() != null) {
+			// String targetName = replaceLast(to.getName(),Files.getFileExtension(to.getName()),"dot"); // getFileExtension nicht in Guava 10,0
+			String targetName = replaceLast(to.getName(), getFileExtension(to.getName()), "dot");
+			File targetFile = new File(getDotDir(), targetName);
+			logger.debug("try to move {} to {}.", dot.getAbsolutePath(), targetFile.getAbsolutePath());
+			Files.move(dot, targetFile);
+		}
+		else
+			deleteDotFile(dot);
 	}
 
-    private static String replaceLast(String string, String toReplace, String replacement) {
-        int pos = string.lastIndexOf(toReplace);
-        if (pos > -1) {
-            return string.substring(0, pos)
-                    + replacement
-                    + string.substring(pos + toReplace.length(), string.length());
-        }
-        return string;
-    }
+	private static String replaceLast(String string, String toReplace, String replacement) {
+		int pos = string.lastIndexOf(toReplace);
+		if (pos > -1) {
+			return string.substring(0, pos) + replacement + string.substring(pos + toReplace.length(), string.length());
+		}
+		return string;
+	}
 
-    private String getFileExtension(String file) {
-        int i = file.lastIndexOf(".");
-        return (i > 0) ? file.substring(i+1) : "???";
-    }
+	private String getFileExtension(String file) {
+		int i = file.lastIndexOf(".");
+		return (i > 0) ? file.substring(i + 1) : "???";
+	}
 
-    private void deleteDotFile(File dotFile) {
-        if (dotFile != null) {
-            if (dotFile.delete() == false)
-                logger.warn(dotFile.getAbsolutePath() + " could not be deleted!");
-        }
-    }
+	private void deleteDotFile(File dotFile) {
+		if (dotFile != null) {
+			if (dotFile.delete() == false)
+				logger.warn(dotFile.getAbsolutePath() + " could not be deleted!");
+		}
+	}
 
 	/**
 	 * Returns the graph as an image in binary format.
@@ -88,11 +102,11 @@ public class GraphIO {
 	 */
 	private byte[] getGraph(File dotFile, FileType type) {
 		byte[] imgStream = null;
-        if (dotFile != null) {
-            imgStream = getImgStream(dotFile, type.name());
-            return imgStream;
-        }
-        return null;
+		if (dotFile != null) {
+			imgStream = getImgStream(dotFile, type.name());
+			return imgStream;
+		}
+		return null;
 	}
 
 	/**
@@ -109,63 +123,58 @@ public class GraphIO {
 	private byte[] getImgStream(File dot, String type) {
 		File img;
 		byte[] img_stream = null;
-
 		try {
 			img = File.createTempFile("graph_", "." + type, new File(getTempDir()));
 			Runtime rt = Runtime.getRuntime();
-
 			// patch by Mike Chenault
 			String[] args = { DOT, "-T" + type, dot.getAbsolutePath(), "-o", img.getAbsolutePath() };
 			Process p = rt.exec(args);
-            String cmd = getCommandString(args);
-            logger.info("Calling: " + cmd);
-
-            InputStream stderrIs = p.getErrorStream();
-            InputStreamReader stderrReader = new InputStreamReader(stderrIs);
-            BufferedReader stderr = new BufferedReader(stderrReader);
-
-            String line = null;
-            while ( (line = stderr.readLine()) != null) {
-                if (line.startsWith("Warning:")) {
-                    logger.warn(line);
-                } else {
-                    logger.error(line);
-                }
-            }
-            int exitVal = p.waitFor();
-            logger.info("Process ends with cc=" + exitVal);
-            if (exitVal != 0)
-                throw new RuntimeException(cmd + " ends with cc=" + exitVal);
-
+			String cmd = getCommandString(args);
+			logger.info("Calling: " + cmd);
+			InputStream stderrIs = p.getErrorStream();
+			InputStreamReader stderrReader = new InputStreamReader(stderrIs);
+			BufferedReader stderr = new BufferedReader(stderrReader);
+			String line = null;
+			while ((line = stderr.readLine()) != null) {
+				if (line.startsWith("Warning:")) {
+					logger.warn(line);
+				}
+				else {
+					logger.error(line);
+				}
+			}
+			int exitVal = p.waitFor();
+			logger.info("Process ends with cc=" + exitVal);
+			if (exitVal != 0)
+				throw new RuntimeException(cmd + " ends with cc=" + exitVal);
 			FileInputStream in = new FileInputStream(img.getAbsolutePath());
 			img_stream = new byte[in.available()];
 			in.read(img_stream);
-
 			// Close it if we need to
 			if (in != null)
 				in.close();
-
 			if (img.delete() == false)
 				System.err.println("Warning: " + img.getAbsolutePath() + " could not be deleted!");
-		} catch (java.io.IOException ioe) {
+		}
+		catch (java.io.IOException ioe) {
 			System.err.println("Error:    in I/O processing of tempfile in dir " + getTempDir() + "\n");
 			System.err.println("       or in calling external command");
 			ioe.printStackTrace();
-		} catch (java.lang.InterruptedException ie) {
+		}
+		catch (java.lang.InterruptedException ie) {
 			System.err.println("Error: the execution of the external program was interrupted");
 			ie.printStackTrace();
 		}
-
 		return img_stream;
 	}
 
-    private String getCommandString(String[] args) {
-        StringBuffer b = new StringBuffer();
-        for(String s : args) {
-            b.append(s + " ");
-        }
-        return b.toString();
-    }
+	private String getCommandString(String[] args) {
+		StringBuffer b = new StringBuffer();
+		for (String s : args) {
+			b.append(s + " ");
+		}
+		return b.toString();
+	}
 
 	/**
 	 * Writes the source of the graph in a file, and returns the written file as
@@ -178,12 +187,13 @@ public class GraphIO {
 	 */
 	private File writeDotSourceToTemporaryFile(String content) throws java.io.IOException {
 		File temp = File.createTempFile("graph_", ".dot.tmp", new File(getTempDir()));
-        try {
+		try {
 			FileWriter fout = new FileWriter(temp);
 			fout.write(content);
 			fout.close();
-		} catch (Exception e) {
-            logger.error("Error: I/O error while writing the dot source to temp file {}.", temp.getAbsolutePath(), e);
+		}
+		catch (Exception e) {
+			logger.error("Error: I/O error while writing the dot source to temp file {}.", temp.getAbsolutePath(), e);
 			return null;
 		}
 		return temp;
@@ -204,6 +214,4 @@ public class GraphIO {
 	public void setDotDir(String dotDir) {
 		this.dotDir = dotDir;
 	}
-
 }
-
